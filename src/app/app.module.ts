547969e9241512aaa4de2921f08f4eb62c0d1876
Shivam Tiwari <shivamtiwari@Shivams-MacBook-Pro.local>
2020-07-22 00:06:10 +0530

    KODO-102: Tard view added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchSortComponent } from './search-sort/search-sort.component';
 import { CardViewComponent } from './card-view/card-view.component';
+import { TableViewComponent } from './table-view/table-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchSortComponent,
     CardViewComponent,
+    TableViewComponent
   ],
   imports: [
     BrowserModule,
